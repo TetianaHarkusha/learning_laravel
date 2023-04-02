0000205314df0d786dcb79f5917b7c4f6cd07230
Tetiana <tatianagarkusha@gmail.com>
2023-04-02 14:21:51 +0200

    Lesson7. Congigurations: Listening For Query Events and paginator

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,10 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Database\Events\QueryExecuted;
+use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,5 +30,15 @@ class AppServiceProvider extends ServiceProvider
     {
         View::share('name', 'John');
         View::share('surname', 'Smit');
+
+        // Listening For Query Events
+        DB::listen(function (QueryExecuted $query) {
+            Log::info("
+            Sql: $query->sql
+            Time: $query->time
+            ------------------");
+        });
+
+        Paginator::useBootstrap();
     }
 }
