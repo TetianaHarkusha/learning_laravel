485547be6a3d9bd44781b1684d29fce205804750
Tetiana <tatianagarkusha@gmail.com>
2023-04-13 15:45:21 +0200

    Rreparing for lesson10

--- resources/views/Pages/Homework/list.blade.php
+++ resources/views/Pages/Homework/list.blade.php
@@ -9,5 +9,6 @@
     <ul>
         <li><a href={{route('homework.lesson', ['id' => 7])}}> Лекція 7. Будівельник запитів</a></li>
         <li><a href={{route('homework.lesson', ['id' => 8])}}> Лекція 8-9. ELOQUENT - початок роботи</a></li>
+        <li><a href={{route('homework.lesson', ['id' => 10])}}> Лекція 10. Eloquent - відносини</a></li>
     </ul>
 @endsection
