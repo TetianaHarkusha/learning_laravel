ae41e6235178a3c630723883a6a71428c4d3fd7c
Tetiana <tatianagarkusha@gmail.com>
2023-04-21 14:18:37 +0200

    Created bashboard main page using layout, includes, sections

---
+++ resources/views/Admin/includes/Sidebar/index.blade.php
@@ -0,0 +1,7 @@
+<div class="sidebar">
+    <!-- Sidebar user panel (optional) -->
+    @include('Admin.includes.sidebar.user')
+    <!-- Sidebar Menu -->
+    @include('Admin.includes.sidebar.menu')
+    <!-- /.sidebar-menu -->
+</div>
---
+++ resources/views/Admin/includes/logo.blade.php
@@ -0,0 +1,4 @@
+<a href={{route('dashboard.main')}} class="brand-link">
+    <img src="Admin/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <span class="brand-text font-weight-light">Адмін.панель</span>
+</a>
