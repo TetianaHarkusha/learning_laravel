3e062e56c5b367b14f1fb23f7df1f5d3fed7b7b4
Tetiana <tatianagarkusha@gmail.com>
2023-04-28 08:46:52 +0200

    Lesson14,pp.1,8-9. Storing and getting values in session using HomeworkController, updeted Homework route

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\CityController;
 use App\Http\Controllers\CountryController;
 use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\TestController;
+use App\Http\Controllers\HomeworkController;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Artisan;
 
@@ -30,12 +31,8 @@ Route::get('/', function () {
 
 // grouped routes 'homework'
 Route::name('homework')->prefix('homework')->group(function () {
-    Route::get('', function () {
-        return view('Pages.homework.list', ['title' => 'Homework']);
-    })->name('.list');
-    Route::get('/{id}', function ($id) {
-        return view('Pages.homework.lesson'. $id, ['title' => 'Homework']);
-    })->name('.lesson');
+    Route::get('', [HomeworkController::class, 'showAll'])->name('.list');
+    Route::get('/{id}', [HomeworkController::class, 'showOne'])->name('.lesson');
 });
 
 //grouped routes 'posts'
@@ -50,11 +47,13 @@ Route::prefix('post')->name('post.')->group(function () {
     Route::get('/{catId}/{postId}', [PostController::class, 'show'])->name('catAndPostId');
 });
 
-// routes for p.3
-Route::match(['get', 'post'], '/test', [TestController::class, 'testForm'])
-    ->middleware('setLocale:uk')->name('test.form');
-
-Route::get('/test/response/{id}', [TestController::class, 'myResponse'])->name('test.response');
+// routes for test
+Route::prefix('test')->name('test.')->group(function () {
+    Route::match(['get', 'post'], '', [TestController::class, 'testForm'])
+    ->middleware('setLocale:uk')->name('form');
+    
+    Route::get('/response/{id}', [TestController::class, 'myResponse'])->name('response');
+});
 
 Route::get('/dir/test', function () {
     return 'Тестова сторінка в dir';
@@ -103,8 +102,10 @@ Route::prefix('user')->name('user.')->group(function () {
         Route::prefix('position')->name('position.')->group(function () {
             Route::get('one', [UserController::class, 'showOneWithCityAndPosition'])
             ->name('one');
+
             Route::get('all', [UserController::class, 'showAllWithCityAndPosition'])
             ->name('all');
+
             Route::get('query/{id}', [UserController::class, 'showWithCityAndPositionQuery'])
             ->name('query');
         });
