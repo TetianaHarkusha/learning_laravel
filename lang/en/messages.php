ef9832ac7bf0207506a432290850ebaf8d5cfaea
Tetiana <tatianagarkusha@gmail.com>
2023-05-18 09:19:32 +0200

    Lesson17. Localization for Authorization, using files within the lang directory.

---
+++ lang/en/messages.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Messages Language Lines
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    'show-dashboard' => 'Sorry, You do not have access to the dashboard.',
+    'policy-post-viewAny' => "Sorry, You do not have access to view posts.",
+    'policy-post-view' => 'Sorry, You do not have access to view this post.',
+    'policy-post-update' => 'Sorry, You do not have access to update this post.',
+    'policy-post-delete' => 'Sorry, You do not have access to delete this post.',
+    'policy-post-create' => 'Sorry, You do not have access to create posts.',
+    'policy-post-restore' => 'Sorry, You do not have access to restore posts.',
+    
+];
